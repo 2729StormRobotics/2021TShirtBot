cbf70e6481957ae063bbe3c52f309dd613e7bf09
Clam <57159181+133622@users.noreply.github.com>
2021-05-11 14:41:07 -0400

    Update Drivetrain and Constants

--- TShirtBot/src/main/java/frc/robot/Constants.java
+++ TShirtBot/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final class DrivetrainConstants {
+        // Assign motor ports  *CHANGE LATER*
+        public static final int kLeftMotorPort1 = 0;
+        public static final int kLeftMotorPort2 = 0;
+        public static final int kRightMotorPort1 = 0;
+        public static final int kRightMotorPort2 = 0;
+    }
+}
--- TShirtBot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ TShirtBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,15 +5,28 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import static frc.robot.Constants.DrivetrainConstants.*;
+
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
 
   // Declare motor objects
+  private final WPI_TalonSRX m_leftMotor1;
+  private final WPI_TalonSRX m_leftMotor2;
+  private final WPI_TalonSRX m_rightMotor1;
+  private final WPI_TalonSRX m_rightMotor2;
 
+  public Drivetrain() {
 
-  public Drivetrain() {}
+    // Initialize motor objects
+    m_leftMotor1 = new WPI_TalonSRX(kLeftMotorPort1);
+    m_leftMotor2 = new WPI_TalonSRX(kLeftMotorPort2);
+    m_rightMotor1 = new WPI_TalonSRX(kRightMotorPort1);
+    m_rightMotor2 = new WPI_TalonSRX(kRightMotorPort2);
+  }
 
   @Override
   public void periodic() {
