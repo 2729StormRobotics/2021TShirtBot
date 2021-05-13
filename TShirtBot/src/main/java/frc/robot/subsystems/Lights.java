ff9ffc93ec6faf10c91ef0e1376b69c6a504bdf0
Sid Parikh <parikhsid16@gmail.com>
2021-05-13 08:53:27 -0400

    Add appropraite LED member varialbes and add methods to change colors of strip

--- TShirtBot/src/main/java/frc/robot/subsystems/Lights.java
+++ TShirtBot/src/main/java/frc/robot/subsystems/Lights.java
@@ -20,8 +20,51 @@ public class Lights extends SubsystemBase {
 
     m_led = new AddressableLED(/* TODO: kLedPort (must be PWM port of Rio) */);
 
+
     // This is an expensive operation and must NOT be done periodically. 
+    // Reuse this buffer. 
     m_ledBuffer = new AddressableLEDBuffer(/* TODO: kLedLength */);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+
+  }
+
+  /**
+   * Sets a section of the LED strip from 0-based index start (inclusive) to index end (exclusive)
+   * to the color specified by r, g, and b. 
+   * If the range specified is invalid, this method does nothing.
+   */
+  public void setSectionRGB(int start, int end, int r, int g, int b) {
+    if (start >= 0 && end > start && end <= m_ledBuffer.getLength()) {
+
+      for (int i = start; i < end; i++) {
+        m_ledBuffer.setRGB(i, r, g, b);
+      }
+
+      m_led.setData(m_ledBuffer);
+
+    }
+
+  }
+
+  /**
+   * Sets a section of the LED strip from 0-based index start (inclusive) to index end (exclusive)
+   * to the color specified by h, s, and v.
+   * If the range specified is invalid, this method does nothing.
+   */
+  public void setSectionHSV(int start, int end, int h, int s, int v) {
+    if (start >= 0 && end > start && end <= m_ledBuffer.getLength()) {
+
+      for (int i = start; i < end; i++) {
+        m_ledBuffer.setHSV(i, h, s, v);
+      }
+
+      m_led.setData(m_ledBuffer);
+
+    }
 
   }
 
